1b047ecb545cac01ff7758ba9c5d206c9453af61
Klrushka <76685224+Klrushka@users.noreply.github.com>
2021-06-04 18:37:43 +0300

    Add files via upload

---
+++ Initialization&Cleanup/src/Task19.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Task19 {
+    static void task19 (String... strings){
+        System.out.println(Arrays.toString(strings));
+    }
+}
---
+++ Initialization&Cleanup/src/Task4.java
@@ -0,0 +1,10 @@
+public class Task4 {
+
+    public Task4() {
+        System.out.println("default constructor");
+    }
+
+    public Task4 (String str) {
+        System.out.println("String from params " + str);
+    }
+}
